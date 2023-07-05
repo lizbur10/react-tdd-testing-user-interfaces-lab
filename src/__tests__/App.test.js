899b7ae53380e98686aa82025a823d5cd248a35d
Liz Burton <liz.burton147@gmail.com>
2023-07-05 16:03:05 -0400

    trying some stuff

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import '@testing-library/jest-dom';
 import App from "../App";
 
 test("displays a top-level heading with the text `Hi, I'm _______`", () => {
